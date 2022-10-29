7e4b9c63dba7df2492fe74241f0e77ad473c7d2a
itsmac <saagarmac@gmail.com>
2022-10-29 22:40:39 +0530

    feat : new logic in progress for refresh

--- scroll.js
+++ scroll.js
@@ -1,10 +1,13 @@
 import { fetchPostsData } from './GeneratePosts.js';
 
-const API_KEY = "yTliB0obDXV6DLvrIBduGZgK6TcCmHx6";
+// const URL =
+//   "https://api.giphy.com/v1/gifs/trending?api_key=" + API_KEY + "&limit=10";
 
-const URL =
-  "https://api.giphy.com/v1/gifs/trending?api_key=" + API_KEY + "&limit=10";
+
 const card = document.querySelector(".card");
+let lastPageNumber = -1;
+let bottomHit = false;
+
 //const gifPreview = document.querySelector(".gifPreview");
 // console.log(card.getBoundingClientRect().bottom)
 // console.log(card.getBoundingClientRect().top)
@@ -20,15 +23,14 @@ const card = document.querySelector(".card");
 // }
 // );
 
-
-
 async function getData(limit = 10, pageNumber) {
   try {
+    //console.log(lastPageNumber);
     //let response = await fetch(URL);
+    // let response;
+    if (pageNumber == lastPageNumber) {return;}
     let response = await fetchPostsData(limit, pageNumber);
-    // console.log("------------------------");
-    // console.log(response);
-    // //let responseJson = await response.json();
+    if (response != []){lastPageNumber = pageNumber}
     printTitle(response);
   }
   catch(err)  {
@@ -46,8 +48,8 @@ function printTitle(val) {
     const articleHeading = document.createElement('h2');
     const articleDescription = document.createElement('p');
 
-    articleHeading.innerHTML = element.title;
-    articleDescription.innerText = element.description;
+    articleHeading.appendChild(document.createTextNode(element.title));
+    articleDescription.appendChild(document.createTextNode(element.description));
     
     article.appendChild(articleHeading);
     article.appendChild(articleDescription);
@@ -57,41 +59,43 @@ function printTitle(val) {
   console.log(parentDiv)
   //card.appe = contentTitle;
   card.append(parentDiv);
+  
   //content += `<div> Title : ${val.title} </div>`
 }
 
-// while(true){
-//    let relativeBottom = document.documentElement.getBoundingClientRect().bottom;
-//    console.log(relativeBottom);
 
-//    let height = document.documentElement.clientHeight;
-//    console.log(height);
-//    if (relativeBottom > document.documentElement.clientHeight){
+function debounce(func, wait) {
+  let timeout;
+  return () => {
+      if (timeout) {
+          clearTimeout(timeout);
+      }
+      timeout = setTimeout(func, wait)
+  }
+}
 
-//    }
-// }
+function popWhenScroll(){
+  let scrollTopVal = document.documentElement.scrollTop;
+  let scrollHeightVal = document.documentElement.scrollHeight;
+  let clientHeightVal = document.documentElement.clientHeight;
+  
+  if (scrollHeightVal - scrollTopVal === clientHeightVal){
+    bottomHit = true;
+  }
+  //else {bottomHit = 0}
+  if (bottomHit == true){
+    bottomHit = false;
+    getData(10,lastPageNumber+1);
+  }
+}
 
-// function popWhenScroll() {
-//   //while (true){
-//   //let relativeBottom = document.documentElement.getBoundingClientRect().bottom;
-//   let height = document.documentElement.clientHeight;
-//   //setVal = 0;
+window.addEventListener("scroll", debounce(popWhenScroll,250));
 
-//   if (relativeBottom < height) {
-//     getData();
-//     //continue;
-//   }
-//   //break
-//   //card.innerHTML += `<div>${new Date()}<div>`
-// }
-// // }``
-// window.addEventListener("scroll", popWhenScroll);
-
-getData(10,2);
+getData(10,lastPageNumber+1);
 
 //
 // get(api),  -> Partially done
-// pagination , 
+// pagination -> Partially done, 
 // render(revamp), -> Partially done
 // refresh(logic change), 
 // try remove event listener
